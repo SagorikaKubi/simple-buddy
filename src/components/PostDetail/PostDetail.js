cf318a30327ad64ab2d74a533a648c529b2f1bff
SagorikaKubi <sagorika.tech@gmail.com>
2021-03-08 22:02:51 +0600

    dependency added

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -12,14 +12,14 @@ const PostDetail = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setPost(data))
-    }, [])
+    }, [id])
 
     useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/posts/1/comments?postId=${id}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setComments(data))
-    }, [])
+    }, [id])
 
     return (
         <div>
